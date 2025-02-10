f44368a742f3888ce6e28939872118b8f98e23b6
dnjsrk <bkw0820@gmail.com>
2025-02-10 23:23:26 +0900

    SWEA2063

---
+++ SWEA/SWEA2063.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SWEA2063 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {  // 선택정렬
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        System.out.println(arr[arr.length / 2]);  // 중간 인덱스에 해당하는 값
+    }
+}
