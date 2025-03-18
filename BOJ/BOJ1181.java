9736eca5140c15373834afd03fc23a8c257c2f29
dnjsrk <bkw0820@gmail.com>
2025-03-18 22:43:41 +0900

    BOJ1181

---
+++ BOJ/BOJ1181.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class BOJ1181 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
+        System.out.println(str[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (!str[i].equals(str[i - 1])) {
+                System.out.println(str[i]);
+            }
+        }
+    }
+}
