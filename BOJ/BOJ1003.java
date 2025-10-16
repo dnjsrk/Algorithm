b6991496159ad3535e05ddae9ebe980c4a5c32ef
dnjsrk <bkw0820@gmail.com>
2025-10-17 00:04:33 +0900

    BOJ1003

---
+++ BOJ/BOJ1003.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1003 {
+    static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        dp[0][0] = 1; dp[0][1] = 0;
+        dp[1][0] = 0; dp[1][1] = 1;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            fibonacci(n);
+            sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static Integer[] fibonacci(int n) {
+        if (dp[n][0] == null || dp[n][1] == null) {
+            dp[n][0] = fibonacci(n - 1)[0] + fibonacci(n - 2)[0];
+            dp[n][1] = fibonacci(n - 1)[1] + fibonacci(n - 2)[1];
+        }
+        return dp[n];
+    }
+}
