8f568a3dd1f28838fe7b6f9f3f782d1ac385d32b
dnjsrk <bkw0820@gmail.com>
2025-10-17 22:53:55 +0900

    BOJ2630

---
+++ BOJ/BOJ2630.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2630 {
+    public static int white = 0;
+    public static int blue = 0;
+    public static int[][] board;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        board = new int[N][N];
+
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        partition(0, 0, N);
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    public static void partition(int row, int col, int size) {
+        if (colorCheck(row, col, size)) {
+            if (board[row][col] == 0) {
+                white++;
+            }
+            else {
+                blue++;
+            }
+            return;
+        }
+
+        int half = size / 2;
+
+        partition(row, col, half);
+        partition(row, col + half, half);
+        partition(row + half, col, half);
+        partition(row + half, col + half, half);
+    }
+
+    public static boolean colorCheck(int row, int col, int size) {
+        int color = board[row][col];
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (board[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
